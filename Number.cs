d433be88f271da641431b7ed40cbb2ea5dffaaf9
dimasfajar12 <80519340+dimasfajar12@users.noreply.github.com>
2021-04-01 18:43:11 +0700

    Add files via upload

---
+++ Number.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LatihanOverloadingMethod
+{
+    public class Number
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            int hasil;
+            if (number2 < number1)
+                hasil = number2;
+            else
+                hasil = number1;
+            return hasil;
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            int hasil;
+            if (number3 < number2)
+                hasil = (int)number3;
+            else
+            if (number2 < number1)
+                hasil = number2;
+            else
+                hasil = number1;
+            return hasil;
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            int hasil;
+            if (number2 > number1)
+                hasil = number2;
+            else
+                hasil = number1;
+            return hasil;
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            int hasil;
+            if (number3 > number2)
+                hasil = (int)number3;
+            else
+                if (number2 > number1)
+                hasil = number2;
+            else
+                hasil = number1;
+            return hasil;
+        }
+    }
+}
